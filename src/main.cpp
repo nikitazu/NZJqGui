2bb6a3b67e99fb2f7db5849cabd705c6d9c734fd
Nikita B. Zuev <nikitazu@gmail.com>
2024-03-01 18:16:12 +0300

    Выделение на стеке - вроде пашет

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,16 @@ int main(int argc, char** argv)
 {
     printf("[INIT] FLTK -- START\n");
 
-    Fl_Window* window = new Fl_Window(800, 600);
-    Fl_Box* box = new Fl_Box(20, 40, 300, 100, "Hello, FLTK!");
+    auto window = Fl_Window(800, 600);
+    auto box = Fl_Box(20, 40, 300, 100, "Hello, FLTK!");
 
-    box->box(FL_UP_BOX);
-    box->labelfont(FL_BOLD + FL_ITALIC);
-    box->labelsize(36);
-    box->labeltype(FL_SHADOW_LABEL);
+    box.box(FL_UP_BOX);
+    box.labelfont(FL_BOLD + FL_ITALIC);
+    box.labelsize(36);
+    box.labeltype(FL_SHADOW_LABEL);
 
-    window->end();
-    window->show(argc, argv);
+    window.end();
+    window.show(argc, argv);
 
     printf("[INIT] FLTK -- OK\n");
 
