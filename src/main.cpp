c74fa32c09cd517f6b796820567ac88acecbd64e
Nikita B. Zuev <nikitazu@gmail.com>
2024-03-01 22:29:24 +0300

    Типа РЕПЛ

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string>
 
 #include <FL/Fl.H>
 #include <FL/Fl_Box.H>
@@ -13,14 +14,17 @@ const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
 void submit_cb(Fl_Widget* w, void* v);
-Fl_Box* _boxRef = nullptr;
+
+Fl_Box* gpBox = nullptr;
+Fl_Multiline_Output* gpOutput = nullptr;
+Fl_Input* gpInput = nullptr;
 
 int main(int argc, char** argv)
 {
     auto window = Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Привет, FLTK [ v0.1.0 ]");
     auto box = Fl_Box(20, 20, 300, 100, "Привет, FLTK!");
 
-    _boxRef = &box;
+    gpBox = &box;
 
     Fl_WindowEx::set_position_center_screen(window);
     window.color(FL_CYAN);
@@ -35,6 +39,9 @@ int main(int argc, char** argv)
     auto input = Fl_Input(100, 340, 800 - 200, 25, "Ввод");
     auto submit = Fl_Button(100, 380, 300, 25, "Отправить");
 
+    gpOutput = &output;
+    gpInput = &input;
+
     submit.callback(submit_cb);
 
     window.end();
@@ -45,9 +52,22 @@ int main(int argc, char** argv)
 
 void submit_cb(Fl_Widget* w, void* v)
 {
-    if (_boxRef != nullptr) {
-        auto new_color = _boxRef->color() == FL_RED ? FL_WHITE : FL_RED;
-        _boxRef->color(new_color);
-        _boxRef->redraw();
+    if (gpBox != nullptr) {
+        auto new_color = gpBox->color() == FL_RED ? FL_WHITE : FL_RED;
+        gpBox->color(new_color);
+        gpBox->redraw();
+    }
+
+    if (gpInput != nullptr && gpOutput != nullptr) {
+        auto input_string = std::string(gpInput->value());
+        auto output_string = std::string(gpOutput->value());
+
+        auto combined_string =
+            output_string.length() > 0
+            ? output_string + "\n" + input_string
+            : input_string;
+
+        gpOutput->value(combined_string.c_str());
+        gpInput->value("");
     }
 }
