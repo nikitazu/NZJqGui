868a5f552f014f45dd502909664b5127407da7c0
Nikita B. Zuev <nikitazu@gmail.com>
2024-03-01 21:48:54 +0300

    Пример обратного вызова

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,15 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
+void submit_cb(Fl_Widget* w, void* v);
+Fl_Box* _boxRef = nullptr;
+
 int main(int argc, char** argv)
 {
     auto window = Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Привет, FLTK [ v0.1.0 ]");
     auto box = Fl_Box(20, 20, 300, 100, "Привет, FLTK!");
+
+    _boxRef = &box;
 
     Fl_WindowEx::set_position_center_screen(window);
     window.color(FL_CYAN);
@@ -30,8 +35,19 @@ int main(int argc, char** argv)
     auto input = Fl_Input(100, 340, 800 - 200, 25, "Ввод");
     auto submit = Fl_Button(100, 380, 300, 25, "Отправить");
 
+    submit.callback(submit_cb);
+
     window.end();
     window.show(argc, argv);
 
     return Fl::run();
+}
+
+void submit_cb(Fl_Widget* w, void* v)
+{
+    if (_boxRef != nullptr) {
+        auto new_color = _boxRef->color() == FL_RED ? FL_WHITE : FL_RED;
+        _boxRef->color(new_color);
+        _boxRef->redraw();
+    }
 }
